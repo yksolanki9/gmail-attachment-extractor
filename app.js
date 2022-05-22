3ee8ac9ceb726d62032dcd6044f66941fbde041b
Yash Solanki <yashsolankli1709@gmail.com>
2022-05-23 04:50:50 +0530

    Add callback route and searchbar in ui

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import { getGoogleAuthURL, getOAuth2Client, getGoogleUser } from './config/google-auth.js';
 import { google } from 'googleapis';
-import './config/mongoose';
+import './config/mongoose.js';
 
 const app = express();
 const gmail = google.gmail('v1');
@@ -23,36 +23,45 @@ app.get('/auth/callback', async(req, res) => {
     const googleUser = await getGoogleUser(req.query);
   
     const { id, email, name } = googleUser.data;
-    console.log('USER ID IS', id);
 
-    // let user = await User.findOne({email});
+    let user = await User.findOne({email});
 
-    // if (!user) {
-    //   user = new User({
-    //     _id: new mongoose.Types.ObjectId(),
-    //     googleId: id,
-    //     name,
-    //     email,
-    //     refresh_token: googleUser.refresh_token
-    //   });
+    if (!user) {
+      user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        googleId: id,
+        name,
+        email,
+        refresh_token: googleUser.refresh_token
+      });
       
-    //   await user.save();
-    // }
+      await user.save();
+    }
 
-    // res.render('auth', {url: `http://localhost:3000/calendar/${user._id}`});
+    res.render('auth', {url: `http://localhost:3000/messages/search/${user._id}`});
 
-    google.options({auth: getOAuth2Client(googleUser.refresh_token)});
+    // google.options({auth: getOAuth2Client(googleUser.refresh_token)});
 
-    const data = await gmail.users.messages.list({
-      userId: email,
-      q: 'has:attachment'
-    });
+    // const data = await gmail.users.messages.list({
+    //   userId: email,
+    //   q: 'has:attachment'
+    // });
 
     // res.send(googleUser.data);
-    res.send(data);
+    // res.send(data);
   } catch(err) {
     res.status(500).json({ error: err.message });
   }
+});
+
+app.get('/messages/search/:userId', async(req, res) => {
+    // google.options({auth: getOAuth2Client(googleUser.refresh_token)});
+
+    // const data = await gmail.users.messages.list({
+    //   userId: email,
+    //   q: 'has:attachment'
+    // });
+    res.render('pages/search-messages');
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'));
