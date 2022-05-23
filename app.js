5638b871feb6e27dc66812e2ad08b2fec67a9b38
Yash Solanki <yashsolankli1709@gmail.com>
2022-05-24 00:57:54 +0530

    Show list of emails and add a separate util file to load attachments

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import 'dotenv/config';
-import { getGoogleAuthURL, getOAuth2Client, getGoogleUser } from './config/google-auth.js';
-import { google } from 'googleapis';
+import { getGoogleAuthURL, getGoogleUser } from './config/google-auth.js';
 import { User } from './models/User.model.js';
 import mongoose from 'mongoose';
 import './config/mongoose.js';
+import { getAttachments } from './utils/get-attachments.js';
 
 const app = express();
-const gmail = google.gmail('v1');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('pages/index');
@@ -40,81 +41,33 @@ app.get('/auth/callback', async(req, res) => {
       await user.save();
     }
     res.redirect(`/messages/search/${user._id}`);
-
-    // Add some page here -> Commented for testing
-    // res.render('auth', {url: `http://localhost:3000/messages/search/${user._id}`});
-
-    // google.options({auth: getOAuth2Client(googleUser.refresh_token)});
-
-    // const data = await gmail.users.messages.list({
-    //   userId: email,
-    //   q: 'has:attachment'
-    // });
-
-    // res.send(googleUser.data);
-    // res.send(data);
   } catch(err) {
     res.status(500).json({ error: err.message });
   }
 });
 
 app.get('/messages/search/:userId', async(req, res) => {
+  const attachments = await getAttachments(req.params.userId, '');
+  const attachmentNames = attachments.map((data) => data.fileName);
+
+  return res.render('pages/search-messages', { data: {
+    attachmentNames
+  }});
+});
+
+app.post('/messages/search/:userId', async(req, res) => {
   // URL FOR TESTING
   // http://localhost:3000/messages/search/628ba16b15244cbf6237821a
 
-  const FILE_FORMAT = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-
   try {
-    const user = await User.findById(req.params.userId);
-    google.options({auth: getOAuth2Client(user.refresh_token)});
+    const attachments = await getAttachments(req.params.userId, req.body.searchQuery);
+    const attachmentNames = attachments.map((data) => data.fileName);
 
-    const messagesList = await gmail.users.messages.list({
-      userId: user.email,
-      q: 'has:attachment',
-      maxResults: 10 //Remove this after testing
-    });
+    // xlsxMessagesData[0].data.data = xlsxMessagesData[0].data.data.replaceAll('-', '+').replaceAll('_', '/');
 
-    const messagesData = await Promise.all(messagesList.data.messages.map(async (message) => {
-      const messageBody = await gmail.users.messages.get({
-        id: message.id,
-        userId: user.email
-      });
-
-      const attachmentDetails = messageBody.data.payload.parts
-        .filter((msgPart) => msgPart.mimeType === FILE_FORMAT && msgPart.body.attachmentId)
-        .map((msgPart) => ({
-          fileName: msgPart.filename,
-          attachmentId: msgPart.body.attachmentId
-        }))[0];
-
-      //This will be undefined when mail has attachment but of different type than pdf
-      if(!attachmentDetails) return;
-
-      const attachment = await gmail.users.messages.attachments.get({
-        id: attachmentDetails.attachmentId,
-        messageId: message.id,
-        userId: user.email
-      });
-
-      const obj = {
-        userId: user.email,
-        messageId: message.id,
-        attachmentId: attachmentDetails.attachmentId,
-        fileName: attachmentDetails.fileName,
-        data: attachment.data,
-      }
-      return obj;
-
-      //Save userId, messageId, filename, attachmentid and attachment data in mongodb
-    }));
-    
-    const pdfMessagesData = messagesData.filter((messageData) => {
-      return !!messageData;
-    });
-
-    // pdfMessagesData[0].data.data = pdfMessagesData[0].data.data.replaceAll('-', '+').replaceAll('_', '/');
-
-    return res.send(pdfMessagesData);
+    return res.render('pages/search-messages', { data: {
+      attachmentNames
+    }});
   } catch(err) {
     res.status(500).json({ error: err.message });
   }
---
+++ utils/get-attachments.js
@@ -0,0 +1,62 @@
+import { google } from 'googleapis';
+import { User } from '../models/User.model.js';
+import { getOAuth2Client } from '../config/google-auth.js';
+
+const gmail = google.gmail('v1');
+
+export async function getAttachments(userId, searchQuery) {
+  const FILE_FORMAT = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+
+  const user = await User.findById(userId);
+  google.options({auth: getOAuth2Client(user.refresh_token)});
+
+  const messagesList = await gmail.users.messages.list({
+    userId: user.email,
+    q: `has:attachment ${searchQuery}`,
+    maxResults: 30 //Remove this after testing
+  });
+
+  if(!messagesList?.data?.messages?.length) {
+    return res.render('pages/search-messages', { data: {
+        errorMsg: 'No Results found'
+      }
+    });
+  }
+
+  const messagesData = await Promise.all(messagesList.data.messages.map(async (message) => {
+    const messageBody = await gmail.users.messages.get({
+      id: message.id,
+      userId: user.email
+    });
+
+    const attachmentDetails = messageBody.data.payload.parts
+      .filter((msgPart) => msgPart.mimeType === FILE_FORMAT && msgPart.body.attachmentId)
+      .map((msgPart) => ({
+        fileName: msgPart.filename,
+        attachmentId: msgPart.body.attachmentId
+      }))[0];
+
+    //This will be undefined when mail has attachment but of different type than xlsx
+    if(!attachmentDetails) return;
+
+    //COMMENTING FOR BETTER PERF
+    // const attachment = await gmail.users.messages.attachments.get({
+    //   id: attachmentDetails.attachmentId,
+    //   messageId: message.id,
+    //   userId: user.email
+    // });
+
+    const obj = {
+      userId: user.email,
+      messageId: message.id,
+      attachmentId: attachmentDetails.attachmentId,
+      fileName: attachmentDetails.fileName,
+      // data: attachment.data,
+    }
+    return obj;
+
+    //Save userId, messageId, filename, attachmentid and attachment data in mongodb
+  }));
+  
+  return messagesData.filter((messageData) => !!messageData);
+}
